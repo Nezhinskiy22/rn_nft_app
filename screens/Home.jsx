a30071e3dd40c77a68d8c5b9b5d3d0a055450fd3
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-05-28 11:48:29 +0300

    Flatlist in progress

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeArea, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HomeHeader, FocusedStatusBar, NFTCard } from "../components";
+
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ fles: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+            right: "0",
+            left: "0",
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, background: COLORS.primary }} />
+          <View style={{ flex: 1, background: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
